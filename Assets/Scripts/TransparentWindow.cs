09577fa8cb005f69767ca6fb55039dfe544ca680
nguyendang111999 <nguyendang111999@gmail.com>
2024-06-09 16:51:44 +0700

    Transparent window

--- Assets/Scripts/TransparentWindow.cs
+++ Assets/Scripts/TransparentWindow.cs
@@ -2,18 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+using System;
 
 public class TransparentWindow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+    [DllImport("user32.dll")]
+    private static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 
+    private struct MARGINS
+    {
+        public int left;
+        public int right;
+        public int top;
+        public int bottom;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    [DllImport("Dwmapi.dll")]
+    private static extern IntPtr DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
 
+    const int GWL_EXSTYLE = -20;
+    const int WS_EX_LAYERED = 0x00080000;
+    const int WS_EX_TRANSPARENT = 0x00000020;
+
+    private void Start()
+    {
+#if !UNITY_EDITOR
+        IntPtr hWnd = GetActiveWindow();
+        MARGINS margins = new MARGINS { left = -1 };
+
+        DwmExtendFrameIntoClientArea(hWnd, ref margins);
+
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+#endif
     }
 }
