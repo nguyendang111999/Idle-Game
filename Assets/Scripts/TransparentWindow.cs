4e33e81b36e99ed8b7896dd04c644fdad6a547c9
nguyendang111999 <nguyendang111999@gmail.com>
2024-06-09 23:33:13 +0700

    Transparent window

--- Assets/Scripts/TransparentWindow.cs
+++ Assets/Scripts/TransparentWindow.cs
@@ -10,7 +10,10 @@ public class TransparentWindow : MonoBehaviour
     private static extern IntPtr GetActiveWindow();
     [DllImport("user32.dll")]
     private static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-
+    [DllImport("user32.dll")]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+    [DllImport("user32.dll")]
+    static extern IntPtr SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte alpha, uint dwFlags);
     private struct MARGINS
     {
         public int left;
@@ -25,16 +28,53 @@ public class TransparentWindow : MonoBehaviour
     const int GWL_EXSTYLE = -20;
     const int WS_EX_LAYERED = 0x00080000;
     const int WS_EX_TRANSPARENT = 0x00000020;
+    static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+    static readonly IntPtr HWND_TOP = new IntPtr(0);
+    const uint LWA_COLORKEY = 0x00000001;
+
+    IntPtr hWnd;
 
     private void Start()
     {
 #if !UNITY_EDITOR
-        IntPtr hWnd = GetActiveWindow();
+        hWnd = GetActiveWindow();
+        // Make window transparent
         MARGINS margins = new MARGINS { left = -1 };
-
         DwmExtendFrameIntoClientArea(hWnd, ref margins);
-
+        // Interact with what's behind this window
         SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        // Display this window above others
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
 #endif
+    }
+
+    [SerializeField] LayerMask _interactLayer;
+
+    private void Update()
+    {
+        // SetClickThrough(Physics2D.OverlapPoint());
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition),
+                            Vector2.zero, 100, _interactLayer);
+
+            SetClickThrough(hits.Length > 0);
+        }
+    }
+
+    public void SetClickThrough(bool value)
+    {
+        if (value)
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+
+            SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
+        }
+        else
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+
+            SetWindowPos(hWnd, HWND_TOP, 0, 0, 0, 0, 0);
+        }
     }
 }
